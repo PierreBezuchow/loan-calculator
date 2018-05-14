42311c1c4cf3d76d6c2769b22fbeb69a3ddefaf2
PierreBezuchow <axel@starosta.pl>
2018-05-14 18:18:14 +0200

    Working calculations and error message that disappears after specific time

--- app.js
+++ app.js
@@ -25,8 +25,36 @@ function calculateResults(e){
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
   } else {
-    console.log('Check numbers');
+    showError('Please check your numbers');
   }
 
   e.preventDefault();
+}
+
+//Show error
+function showError(error) {
+
+  //Create a div
+  const errorDiv = document.createElement('div');
+
+  //Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  //Add class
+  errorDiv.className = 'alert alert-danger';
+
+  //Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  //Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  //Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+//Clear Error
+function clearError() {
+  document.querySelector('.alert').remove();
 }
